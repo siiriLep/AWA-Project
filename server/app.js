8f8297b0c180fa83d57cc3962b7a8e14fe3ea990
Siiri <siiri.leppalampi@gmail.com>
2024-02-13 11:38:27 +0200

    added backend

---
+++ server/app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var api = require('./routes/api');
+var cors = require('cors')
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const mongoose = require("mongoose");
+const mongoDB = "mongodb://127.0.0.1:27017/testdb"
+mongoose.connect(mongoDB)
+mongoose.Promise = Promise
+const db = mongoose.connection
+db.on("error", console.error.bind(console, "database error"))
+
+if (process.env.NODE_ENV === "development") {
+    var corsOptions = {
+        origin: "http//localhost:3000",
+        optionsSuccessStatus: 200,
+    }
+    app.use(cors(corsOptions))
+}
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api', api)
+module.exports = app;
